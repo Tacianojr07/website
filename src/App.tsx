1b198bcd1e642cd5a62519c56e3a52b7c9a9aae3
Tacianojr07 <t.juniior13@gmail.com>
2023-11-18 20:20:28 -0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-function App() {
-  return (
-    <div>
-      <h1>page home</h1>
-    </div>
-  );
-}
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./components/layout";
+import { Home } from "./pages/home";
+import { About } from "./pages/about";
+import { Contact } from "./pages/contact";
+import { Services } from "./pages/services";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "services",
+        element: <Services />,
+      },
+    ],
+  },
+]);
+
+export { router };
