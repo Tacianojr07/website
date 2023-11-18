8ffb9d0d7a3a09680471bc77c969f5182a445855
Tacianojr07 <t.juniior13@gmail.com>
2023-11-18 20:49:23 -0300

    html page home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,28 @@
+import { Container } from "../../components/container";
+
 export function Home() {
   return (
-    <div>
-      <h1>page home</h1>
-    </div>
+    <Container>
+      <main>
+        <section>
+          <div>
+            <h1>Your business sucess start here</h1>
+            <span>
+              Grown your client base with Holihili and targeted strategires
+            </span>
+          </div>
+          <div>
+            <button>Get my Free Proposal</button>
+            <button>how it works</button>
+          </div>
+        </section>
+        <aside>
+          <img
+            src="../../assets/stephen-phillips-hostreviews-co-uk-shr_Xn8S8QU-unsplash.jpg"
+            alt="imagem de negocios"
+          />
+        </aside>
+      </main>
+    </Container>
   );
 }
