6b1d6edafcbbf552240ff1aaa92c265b5b6df45d
Tacianojr07 <t.juniior13@gmail.com>
2023-11-18 20:35:03 -0300

    styled header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <div>
-      <header>
-        <Link to={"/"}>HOLIHLI</Link>
+    <div className="w-full h-16 mx-auto flex justify-center items-center my-5">
+      <header className="w-full max-w-7xl flex items-center justify-start gap-11  mb-4 mx-auto font-medium">
+        <Link className="text-3xl" to={"/"}>
+          HOLIHLI
+        </Link>
         <Link to={"/"}>Home</Link>
         <Link to={"/about"}>About</Link>
         <Link to={"/services"}>Services</Link>
