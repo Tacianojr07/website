6b85b4a5a7a831631b27f306cbe5340c983eeb81
Tacianojr07 <t.juniior13@gmail.com>
2023-11-19 14:26:01 -0300

    html and css page about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,25 @@
+import { Container } from "../../components/container";
+import imgbuniness from "../../assets/woman.jpg";
+import { Button } from "../../components/button";
+
 export function About() {
   return (
-    <div>
-      <h1>page about</h1>
-    </div>
+    <Container>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <img className="rounded-2xl" src={imgbuniness} alt="buniness" />
+        <article className="w-full flex flex-col justify-center items-center mb-5">
+          <p className="text-7xl font-medium">Why need digital</p>
+          <p className="text-6xl font-medium text-zinc-200 mb-10">
+            marketing services
+          </p>
+          <p className="text-xl text-center text-zinc-500">
+            The online marketing indutry is growing at an unprecedented rate.
+            Accoding to eMarketer, businesses in the U.S spend more tanh $110
+            bilion on digital advertising
+          </p>
+          <Button bgColor="#86EFAC" placeholder="Read More"></Button>
+        </article>
+      </section>
+    </Container>
   );
 }
