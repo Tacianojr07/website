3dd7d246f9fcc1952e381948fdc61a115fdc6533
Tacianojr07 <t.juniior13@gmail.com>
2023-11-18 23:06:48 -0300

    html and css page home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,41 @@
 import { Container } from "../../components/container";
+import image from "../../assets/img.jpg";
 
 export function Home() {
   return (
     <Container>
-      <main>
-        <section>
-          <div>
-            <h1>Your business sucess start here</h1>
-            <span>
+      <main className="grid grid-cols-1 lg:grid-cols-2">
+        <section className="w-full max-w-3xl flex justify-center  mt-10 ">
+          <div className="flex flex-col item-center mb-4">
+            <h1 className="text-7xl font-semibold mb-4">
+              Your business sucess start here
+            </h1>
+            <span className="font-thin text-xl">
               Grown your client base with Holihili and targeted strategires
             </span>
-          </div>
-          <div>
-            <button>Get my Free Proposal</button>
-            <button>how it works</button>
+
+            <div className="flex items-start w-full max-w-xl gap-10  mt-5 text-center">
+              <button className=" bg-green-300 w-full py-2 h-10 rounded-full  text-center mx-auto">
+                Get my Free Proposal
+              </button>
+              <button className="bg-slate-200 w-full py-2 h-10 rounded-full   text-center">
+                how it works
+              </button>
+            </div>
           </div>
         </section>
-        <aside>
+        <aside className="w-full ">
           <img
-            src="../../assets/stephen-phillips-hostreviews-co-uk-shr_Xn8S8QU-unsplash.jpg"
+            className="w-full  rounded-lg mb-2 min-h-full hover:scale-105 cursor-pointer transition-all "
+            src={image}
             alt="imagem de negocios"
           />
         </aside>
+
+        <div className="flex flex-col jusitfy-center  gap-2">
+          <span className="text-xl">Social Media / Content Writing / SEOS</span>
+          <span className="text-xl">Video Prodution / Pay Per Click</span>
+        </div>
       </main>
     </Container>
   );
